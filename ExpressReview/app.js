a8365033f39618d6acd47964d4657f6bd521f952
vionaosiako <viona.osiako@student.moringaschool.com>
2023-06-22 19:51:44 +0300

    create a port number 8080

--- ExpressReview/app.js
+++ ExpressReview/app.js
@@ -1,3 +1,10 @@
 //nmp install express
 //Import the express module Express module
 const express = require ('express');
+//Create an express application: create an instance of the Express application by calling express function
+const app = express();
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Server on ${port}`);
+})
