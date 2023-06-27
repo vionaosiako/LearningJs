cd05bf827123c23a36d86d32fb2ed611bcedfb2a
vionaosiako <viona.osiako@student.moringaschool.com>
2023-06-27 20:37:35 +0300

    patch: post error on users

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 //middleware
 app.use(express.urlencoded({extended:true}));
+app.use(express.json);
 
 app.get("/sports", (req,res)=>{
     res.send("Welcome to sport arena!!!");
@@ -20,9 +22,37 @@ app.get("/cal", (req,res)=>{
 app.post("/cal",(req,res)=>{
    const num1 = Number(req.body.num1);
    const num2 = Number(req.body.num2);
+   //performance the calculation
    const addition = num1 + num2;
+   // return a respond of the sum after calculation
    res.send(`The Sum is ${addition}`);
 });
+let users = [
+    {
+        "firstname": "Viona",
+        "lastname": "Osiako",
+        "age": 24
+    },
+    {
+        "firstname": "Jonathan",
+        "lastname": "Omondi",
+        "age": 28
+    }
+]
+
+app.get("/users", (req,res)=>{
+    res.send(users)
+});
+
+app.post("/users", (req,res)=>{
+    const createdUser = req.body
+    const userId = uuidv4();
+    const userWithId = {...createdUser, id: userId};
+
+    users.push(userWithId);
+    res.send(`User with name ${createdUser.firstname} created`)
+});
+
 
 app.listen(5000,()=>{
     console.log("listening on port 5000")
